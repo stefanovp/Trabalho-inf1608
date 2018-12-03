0faa0872a033b7eb688097ee203ae4ed0f9e65c6
TRibeiral <thiagoribeiral98@gmail.com>
2018-12-03 19:58:32 -0200

    Analise

--- main.c
+++ main.c
@@ -1,58 +1,66 @@
 #include <stdio.h>
 #include <math.h>
 #include "pendulo.h"
+#include <sys/time.h>
 
+void getPeriodoPassoConstante(double* theta,double h);
 int main(void)
 {
-	//essa main não é' minha, mas serve para testar. Espero que não seja um problema.
-	double Tc;
-	double T;
-	double theta = 1;
-	printf("Teste para Theta = 1\n");
-	T = periodo_simplificado(theta);
-	Tc = periodo (theta);
-	printf("Periodo da formula: %lf\n",T);
-	printf("Periodo: %lf\n\n",Tc);
+	double theta[] = {PI/180,PI/60,PI/36,PI/18,PI/6,PI/3,PI/2};
+	struct timeval start, stop;
+	double num;
+	long milisecs;
+	
+	printf("Comparacao do valor do periodo calculado\n");
 
-	printf("Teste para Theta = 3\n");
-	theta=3;
-	T = periodo_simplificado(theta);
-	Tc = periodo (theta);
-	printf("Periodo da formula: %lf\n",T);
-	printf("Periodo: %lf\n\n",Tc);
+	printf("--------------Simplificado--------------\n");
+	gettimeofday(&start, NULL);
+	num = periodo_simplificado ();
+	gettimeofday(&stop, NULL);
+	milisecs = (stop.tv_usec - start.tv_usec)/1000 + 1000* (stop.tv_sec - start.tv_sec);
+	
+	printf("Simplificada %.16lf\tTempo (ms) %ld\n",num,milisecs);
+	printf("----------------------------------------\n");
 
-	printf("Teste para Theta = 5\n");
-	theta=5;
-	T = periodo_simplificado(theta);
-	Tc = periodo (theta);
-	printf("Periodo da formula: %lf\n",T);
-	printf("Periodo: %lf\n\n",Tc);
+	printf("-----------------h=0.01-----------------\n");
+	getPeriodoPassoConstante(theta,0.01);
+	printf("----------------------------------------\n");
 
-	printf("Teste para Theta = 10\n");
-	theta=10;
-	T = periodo_simplificado(theta);
-	Tc = periodo (theta);
-	printf("Periodo da formula: %lf\n",T);
-	printf("Periodo: %lf\n\n",Tc);
 
-	printf("Teste para Theta = 30\n");
-	theta=30;
-	T = periodo_simplificado(theta);
-	Tc = periodo (theta);
-	printf("Periodo da formula: %lf\n",T);
-	printf("Periodo: %lf\n\n",Tc);
+	printf("-----------------h=0.001----------------\n");
+	getPeriodoPassoConstante(theta,0.001);
+	printf("----------------------------------------\n");
 
-	printf("Teste para Theta = 60\n");
-	theta=60;
-	T = periodo_simplificado(theta);
-	Tc = periodo (theta);
-	printf("Periodo da formula: %lf\n",T);
-	printf("Periodo: %lf\n\n",Tc);
 
-	printf("Teste para Theta = 90\n");
-	theta=90;
-	T = periodo_simplificado(theta);
-	Tc = periodo (theta);
-	printf("Periodo da formula: %lf\n",T);
-	printf("Periodo: %lf\n\n",Tc);
+	printf("----------------h=0.0001----------------\n");
+	getPeriodoPassoConstante(theta,0.0001);
+	printf("----------------------------------------\n");
+
+
+	printf("---------------Adaptativo---------------\n");
+	for(int i=0;i<7;i++){
+		gettimeofday(&start, NULL);
+		num = adaptPeriodo (theta[i],0.00001);
+		gettimeofday(&stop, NULL);
+		milisecs = (stop.tv_usec - start.tv_usec)/1000 + 1000* (stop.tv_sec - start.tv_sec);
+		printf("Theta %lf\tResultado %.16lf\tTempo (ms) %ld\n",theta[i],num,milisecs);
+	}
+	printf("----------------------------------------\n");
+
+	printf("Angulo inicial maximo para simplifiacao ter erro menor que 0.001\n");
+	printf("%.5lf\n", anguloMax(0.00001) );
+
+}
+
+void getPeriodoPassoConstante(double* theta,double h){
+	double num;
+	struct timeval start, stop;
+	long milisecs;
+	for(int i=0;i<7;i++){
+		gettimeofday(&start, NULL);
+		num = periodo (theta[i],h);
+		gettimeofday(&stop, NULL);
+		milisecs = (stop.tv_usec - start.tv_usec)/1000 + 1000* (stop.tv_sec - start.tv_sec);
+		printf("Theta %lf\tResultado %.16lf\tTempo (ms) %ld\n",theta[i],num,milisecs);
+	}
 }
--- pendulo.c
+++ pendulo.c
@@ -1,59 +1,109 @@
+#include <stdio.h>
 #include <math.h>
 #include "pendulo.h"
-#include <stdio.h>
-#define G 9.8
-#define L 10.0
-#define PI 3.14159265358979323846
 
-double pendulo (double t, double h, double* theta, double* w){
+double x(double theta){
+	double g=9.8;
+	double l=10;
+	return (-g/l)*sin(theta);
+}
+
+double pendulo (double t, double h, double* theta, double* w)
+{
+	double kt1, kt2, kt3, kt4;
+	double kw1,kw2,kw3,kw4;
 	
-	double k1[2], k2[2], k3[2], k4[2];
-
-	k1[0] = *w*h;
-	k1[1] = (-(G/L)*sin(*theta))*h;
-
-	k2[0] = (*w + k1[1]/2)*h;
-	k2[1] = (-(G/L)*sin(*theta + k1[0]/2))*h;
-
-	k3[0] = (*w + k2[1]/2)*h;
-	k3[1] = (-(G/L)*sin(*theta + k2[0]/2))*h;
-
-	k4[0] = (*w + k3[1])*h;
-	k4[1] = (-(G/L)*sin(*theta + k3[0]))*h;
-
-	*theta = *theta + (k1[0] + 2*k2[0] + 2*k3[0] + k4[0])/6;
-	*w = *w + (k1[1] + 2*k2[1] + 2*k3[1] + k4[1])/6;
+	kt1=h*(*w);
+	kw1=h*x(*theta);
+	
+	kt2=h*(*w+kw1/2);
+	kw2=h*x(*theta+kt1/2);
+	
+	kt3=h*(*w+kw2/2);
+	kw3=h*x(*theta+kt2/2);
+	
+	kt4=h*(*w+kw3);
+	kw4=h*x(*theta+kt3);
+	
+	*theta=*theta +(kt1+2*kt2+2*kt3+kt4)/6;
+	*w=*w +(kw1+2*kw2+2*kw3+kw4)/6;
 
 	return t+h;
-
 }
 
-double periodo (double theta_0){
+double periodo (double theta_0,double h)
+{
+	double t1,t2,w1,w2 = 0;
+	int cont=0;	
 
-	double theta=theta_0;
-	double T=0, t1=0, t2=0, w1=0, w2=0, h=0.001;
-	int i=0;
-
-	theta=theta*PI/180;
-	
-	//10 inversoes
-	while(i<10){
+	while(cont<10)
+	{
 		w1=w2;
 		t1=t2;
-
-		t2=pendulo (t1,h, &theta, &w2);
-
+		t2=pendulo (t1,h, &theta_0,&w2);
 		if(w1*w2<0){
-			i++;
+			cont++;
 		}
 	}
-
-	T=(2*(t1 +(fabs(w1)/(fabs(w1)+fabs(w2)))*(t2-t1)));
-
-	return T/10;
-
+	return 	(t1 +(fabs(w1)/(fabs(w1)+fabs(w2)))*(t2-t1))/5;
 }
 
-double periodo_simplificado (double theta0){
-	return 2*PI*sqrt(L/G);
+double periodo_simplificado (void)
+{
+	return 2*PI*sqrt(10/9.8);
 }
+
+double adaptPendulo (double t, double h, double* theta, double* w, double e)
+{
+	double theta1 = *theta;
+	double w1=*w;
+	double t1;
+	double alpha; 
+
+	t1 = pendulo(t,h, &theta1, &w1);
+
+	alpha = sqrt(e/fabs(theta1 - *theta));
+
+	if(alpha < 1)
+		return adaptPendulo (t, alpha*h, theta, w, e);
+	*theta = theta1;
+	*w = w1;
+	return t1;
+}
+
+double adaptPeriodo (double theta_0,double e)
+{
+	double t1,t2,w1,w2 = 0;
+	int cont=0;	
+
+	while(cont<10)
+	{
+		w1 = w2;
+		t1 = t2;
+		t2 = adaptPendulo (t1,0.01, &theta_0,&w2,e);
+		if(w1*w2<0){
+			cont++;
+		}
+	}
+	return 	(t1 +(fabs(w1)/(fabs(w1)+fabs(w2)))*(t2-t1))/5;
+}
+
+
+double anguloMax(double e){
+	double b = PI/2;
+	double a = 0;
+	double erro;
+	double med;
+	while(1){
+		med = (a+b)/2;
+		erro = (b-a)/2;
+		if(erro <e)
+			return med;
+		if(fabs(periodo_simplificado()-adaptPeriodo(med,e))<0.001)
+			a= med;
+		else
+			b=med;
+		//printf("%.16lf\t", med);
+		//printf("%.16lf\n", erro);
+	}
+} 
