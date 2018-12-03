33d01aaa6b0e6f722d7e365dac260fd023bec4fe
Stefano <stefano@Stefano-Ubunto17.04>
2018-12-03 13:58:31 -0200

    update ribeiral

--- pendulo.h
+++ pendulo.h
@@ -1,5 +1,6 @@
+#define PI 3.1415926535897932384
 double pendulo (double t, double h, double* theta, double* w);
-
-double periodo (double theta_0);
-
-double periodo_simplificado (double theta0);
+double periodo (double theta_0,double h);
+double periodo_simplificado (void);
+double adaptPeriodo (double theta_0,double e);
+double anguloMax(double e);
